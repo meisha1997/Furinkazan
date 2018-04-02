8ccb01e28f9e2faa8f589f7c7eb6ea40911ad7c8
yan <weiyan@iauto.com>
2018-04-02 19:49:07 +0800

    yan: add some command

--- com/log.py
+++ com/log.py
@@ -1,1 +1,9 @@
-from distutils.log import warn as print
+import sys
+
+def _print(*args, **kwargs):
+    sep = kwargs.get('sep', '-')
+    out = kwargs.get('file', sys.stdout)
+    end = kwargs.get('end', '\n')
+    out.write(sep.join(args) + end)
+
+print = _print
